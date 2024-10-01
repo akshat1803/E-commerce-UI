d6586cfc52970289d47fb919ebab50cec87b25ce
dheeraj1566 <dheerajjangid013@gmail.com>
2024-10-01 09:55:18 +0530

    changes

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,28 +1,3 @@
-// // src/context/CartContext.js
-// import React, { createContext, useState } from 'react';
-
-// export const CartContext = createContext();
-
-// export const CartProvider = ({ children }) => {
-//     const [cartItems, setCartItems] = useState([]);
-
-//     const addToCart = (product) => {
-//         setCartItems((prevItems) => [...prevItems, product]);
-//     };
-
-//     const removeFromCart = (id) => {
-//         setCartItems((prevItems) => prevItems.filter(item => item.id !== id));
-//     };
-
-//     return (
-//         <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
-//             {children}
-//         </CartContext.Provider>
-//     );
-// };
-
-
-// src/context/CartContext.jsx
 import React, { createContext, useState } from 'react';
 
 export const CartContext = createContext();
